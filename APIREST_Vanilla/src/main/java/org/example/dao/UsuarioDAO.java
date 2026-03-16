7e50302ae8583b904a464fd9a48f32d0e2c7a80a
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-03-16 16:31:02 -0300

    Encriptacion de contraseña

--- APIREST_Vanilla/src/main/java/org/example/dao/UsuarioDAO.java
+++ APIREST_Vanilla/src/main/java/org/example/dao/UsuarioDAO.java
@@ -2,6 +2,7 @@ package org.example.dao;
 
 import org.example.config.DatabaseConnection;
 import org.example.model.Usuario;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,8 +16,11 @@ public class UsuarioDAO {
         try (Connection conn = DatabaseConnection.getConection();
              PreparedStatement statement = conn.prepareStatement(sql))
         {
+            //Encriptar contraseña
+            String hashPassword = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt());
+
             statement.setString(1, usuario.getUsername());
-            statement.setString(2, usuario.getPassword());
+            statement.setString(2, hashPassword);
 
             int filas = statement.executeUpdate();
             if (filas > 0) {
