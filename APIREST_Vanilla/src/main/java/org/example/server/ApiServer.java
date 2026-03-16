335e2e32d832a3abd3bde594d2f586add7fd6b18
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-03-16 17:14:06 -0300

    Update ApiServer.java

--- APIREST_Vanilla/src/main/java/org/example/server/ApiServer.java
+++ APIREST_Vanilla/src/main/java/org/example/server/ApiServer.java
@@ -1,6 +1,7 @@
 package org.example.server;
 
 import com.sun.net.httpserver.HttpServer;
+import org.example.handler.LoginHandler;
 import org.example.handler.UsuarioHandler;
 
 import java.net.InetSocketAddress;
@@ -11,7 +12,11 @@ public class ApiServer {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         // asocia la ruta /usuarios con tu lógica de registro.
-        server.createContext("/usuarios", new UsuarioHandler());
+        server.createContext("/registro", new UsuarioHandler());   // POST registro
+        server.createContext("/login", new LoginHandler());        // POST login
+//        server.createContext("/usuarios/list", new UsuarioListHandler()); // GET lista
+//        server.createContext("/usuarios/get", new UsuarioGetHandler());   // GET por id
+
 
         server.setExecutor(null); //usa el executor por defecto
 
