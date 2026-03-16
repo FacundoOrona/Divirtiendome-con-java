e1f0686e9aed89b25d7bc6678e804a0f0185a985
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-03-16 17:14:02 -0300

    Update UsuarioDAO.java

--- APIREST_Vanilla/src/main/java/org/example/dao/UsuarioDAO.java
+++ APIREST_Vanilla/src/main/java/org/example/dao/UsuarioDAO.java
@@ -6,6 +6,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioDAO {
@@ -30,5 +31,21 @@ public class UsuarioDAO {
         }
     }
 
+    public Usuario buscarPorUsername(String username) throws SQLException {
+        String sql = "SELECT id, username, password FROM usuarios WHERE username = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Usuario usuario = new Usuario(rs.getString("username"), rs.getString("password"));
+                usuario.setId(rs.getInt("id"));
+                return usuario;
+            }
+        }
+        return null; // no encontrado
+    }
 
 }
